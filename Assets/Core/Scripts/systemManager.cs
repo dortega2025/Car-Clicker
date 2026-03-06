8b2bcee2e58d372baf678e278357af6ec2d15aa3
Daniel Ortega <dortega1016@gmail.com>
2026-03-05 18:48:17 -0600

    Added car and track sprites, added system manager

---
+++ Assets/Core/Scripts/systemManager.cs
@@ -0,0 +1,29 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class systemManager : MonoBehaviour
+{
+    public int points;
+    private int increaseFactor;
+    public TMP_Text pointCnt;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        points = 0;
+        increaseFactor = 1;
+        pointCnt = GameObject.Find("points").GetComponent<TMP_Text>();
+        pointCnt.text = "Points: " + points;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pointCnt.text = "Points: " + points;
+    }
+
+    public void increasePoints()
+    {
+        points += increaseFactor;
+    }
+}
