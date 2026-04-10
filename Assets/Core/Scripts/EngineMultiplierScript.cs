6925b3de988a8137206b45dc0467b5a63d96ba2c
Benjamin-Tindall <benjaminktindall@gmail.com>
2026-04-09 19:26:10 -0500

    Changes 4/9/2026
    Added Pricing

--- Assets/Core/Scripts/EngineMultiplierScript.cs
+++ Assets/Core/Scripts/EngineMultiplierScript.cs
@@ -1,26 +1,42 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.UIElements;
+using TMPro;
 public class EngineMultiplierScript : MonoBehaviour
 {
     private int  val = 0;
+    private int cost = 50;
      private float[] engineMultiplier = {1.0f,1.2f,2.5f,4.0f,8.0f,16.0f,32.0f};
     GameObject player;
+    GameObject ppoints;
+    public TMP_Text EngineCost;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
         //grab engineFactor from car
         player = GameObject.FindWithTag("Player");
+        ppoints = GameObject.Find("systemManager");
         
         
         player.GetComponent<car>().engineFactor = engineMultiplier[0];
+        EngineCost = GameObject.Find("Engine Cost").GetComponent<TMP_Text>();
+        EngineCost.text = "Cost: " + cost;
         Debug.Log(engineMultiplier[0]);
         Debug.Log(player.GetComponent<car>().engineFactor);
         Debug.Log(val);
     }
     public void OnClick(){
-        val++;
-        
-        player.GetComponent<car>().engineFactor = engineMultiplier[val];
+        if(ppoints.GetComponent<systemManager>().points>=cost && val <=6){
+            val++;
+            player.GetComponent<car>().engineFactor = engineMultiplier[val];
+            ppoints.GetComponent<systemManager>().points -= cost;
+            cost = cost*2;
+            Debug.Log(cost);
+            EngineCost.text = "Cost: " + cost;
+        }
+        else{
+            EngineCost.text = "Maxed Out!";
+        }
 
         Debug.Log(val);
     }
--- Assets/Core/Scripts/TiresSumScript.cs
+++ Assets/Core/Scripts/TiresSumScript.cs
@@ -1,21 +1,33 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.UIElements;
+using TMPro;
 public class TiresSumScript : MonoBehaviour
 {
     private float TiresSum = 0.0f;
+    private int cost = 15;
+    GameObject ppoints;
     GameObject player;
+    public TMP_Text TireCost;
     void Start()
     {
       player = GameObject.FindWithTag("Player");
+      ppoints = GameObject.Find("systemManager");
       player.GetComponent<car>().tires = TiresSum;  
+      TireCost = GameObject.Find("Tires Cost").GetComponent<TMP_Text>();
+      TireCost.text = "Cost: " + cost;
     }
 
     public void OnClick()
     {
-        TiresSum++;
+        if(ppoints.GetComponent<systemManager>().points>=cost){
+            TiresSum++;
+            player.GetComponent<car>().tires = TiresSum;
+            Debug.Log(TiresSum);  
+            ppoints.GetComponent<systemManager>().points -= cost;
+            cost = cost*2; 
+            TireCost.text = "Cost: " + cost;
+        }
         
-        player.GetComponent<car>().tires = TiresSum;
-
-        Debug.Log(TiresSum);
     } 
 }
